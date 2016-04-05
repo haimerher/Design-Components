138d6562fb69ab1d287042b9a72a1bcba978db40
Hairo Mercedes Hernández <h.mercedes@sisalril.gov.do>
2016-04-05 17:32:35 -0400

    update

--- public/assets/js/ds-scripts.js
+++ public/assets/js/ds-scripts.js
@@ -1,11 +1,17 @@
 $(document).ready(function(){
 
 	//tooltips
+	$("body .tooltip").before('<div class="wrap-tooltip">');
+
 	$("body").on('mouseover', '.tooltip', function(){
 		var tooltip = $(".tooltip").data("tooltip");
 		var tooltipCont = '<span class="tooltip-cont">' + tooltip + '</span>';
+		var x = $('.tooltip').offset();
+		var w = $('.tooltip').width();
+		// $(tooltipCont).hide().preppend('.tooltip').fadeIn(80);
 
-		$(tooltipCont).hide().prependTo('.tooltip').fadeIn(80);
+		$('.tooltip').before(tooltipCont);
+		$('.tooltip-cont').offset({ top: x.top+50, left: x.left });
 	})
 	$("body").on('mouseout', '.tooltip', function(){
 		$('.tooltip-cont').fadeOut(80, function() {
